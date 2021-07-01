95aea2c17680fc55e1e52e325a5ab980c278215d
OrdosX <i@ordosx.tech>
2021-07-01 13:53:32 +0800

    添加辅助输出

--- crawler/update.sh
+++ crawler/update.sh
@@ -2,6 +2,9 @@
 CURRENT_VERSION=$(cat latestVersion.txt)
 DEPLOYED_VERSION=$(curl -s https://getl2d.ordosx.tech/latestVersion.txt)
 
+echo $CURRENT_VERSION
+echo $DEPLOYED_VERSION
+
 if [ $DEPLOYED_VERSION = $CURRENT_VERSION ];
 then
     mkdir files

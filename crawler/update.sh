963860201cea40699c1eb4acc58d0ce7c87e6c34
OrdosX <i@ordosx.tech>
2021-07-01 13:41:26 +0800

    添加上传功能

--- crawler/update.sh
+++ crawler/update.sh
@@ -4,5 +4,8 @@ DEPLOYED_VERSION=$(curl -s https://getl2d.ordosx.tech/latestVersion.txt)
 
 if [ $DEPLOYED_VERSION = $CURRENT_VERSION ];
 then
-    wget -i latestURL.txt
+    mkdir files
+    cd files
+    wget -i ../latestURL.txt
+    ossutil cp -rf . oss://getl2d-ordosx/
 fi

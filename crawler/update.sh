ed1a3ae0ec649a4157136b27d9e8249a6e2c2e5b
OrdosX <i@ordosx.tech>
2021-07-01 15:25:08 +0800

    优化wget输出并修正上传位置

--- crawler/update.sh
+++ crawler/update.sh
@@ -2,13 +2,9 @@
 CURRENT_VERSION=$(cat latestVersion.txt)
 DEPLOYED_VERSION=$(curl -s https://getl2d.ordosx.tech/latestVersion.txt)
 
-echo $CURRENT_VERSION
-echo $DEPLOYED_VERSION
-
 if [ $DEPLOYED_VERSION != $CURRENT_VERSION ];
 then
     mkdir files
-    cd files
-    wget -i ../latestURL.txt
-    ossutil cp -rf . oss://getl2d-ordosx/
+    wget -q -i latestURL.txt -P files/
+    ossutil cp -rf files/ oss://getl2d-ordosx/files/
 fi
